a8bbc60d3e264564be65cd06267c241836b30ff8
Jeff <jeff@fluid.(none)>
2009-12-24 16:03:24 -0500

    Initial commit. Base code from clj-blog.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject hui/clj-mongodb "0.1.0"
+  :description "A light-weight wrapper to the MongoDB java driver."
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+		 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+		 [org.mongodb/mongo-java-driver "1.1"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
+  :url "http://github.com/jeffh/clj-mongodb/")
